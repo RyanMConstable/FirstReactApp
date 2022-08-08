e0bb9b05a657a960a9e325a5cec578032a0007f6
root <root@DESKTOP-MFFLRFR.localdomain>
2022-08-08 02:27:30 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,234 @@
+import './App.css';
+import {useState} from "react";
+import Axios from "axios";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function App() {
+
+
+  //for sizer database
+  const [site, setSite] = useState();
+  const [location, setLocation] = useState();
+  const [numofusers, setNumofusers] = useState();
+  const [totalamountofdata, setTotalamountofdata] = useState();
+  const [estimatedgrowth, setEstimatedgrowth] = useState();
+  const [averagefilesize, setAveragefilesize] = useState();
+  const [numfiles, setNumfiles] = useState();
+  const [customer, setCustomer] = useState();
+  const [tempcustomer, setTempCustomer] = useState();
+
+
+  //List of employees for the print
+  const[employeeList, setEmployeeList] = useState([]);
+
+  //Deleting a row in the database table
+  const [deleteSite, setDeletesite] = useState();
+
+  //Searching rows in the database table
+  ////////////////////////////////////////
+  const [searchSite, setSearchSite] = useState();
+
+
+
+  //sends information from the front end to the backend
+  //After sending the info to the backend it then returns
+  //All table data from the backend and displays it nicely
+  const addSite = () => {console.log(site);
+    //Checks if the variables are empty and displays an alert for now
+    //The error message doesn't display exactly what the issue is as of now
+    if (site == null ||
+        location == null ||
+        numofusers == null ||
+        totalamountofdata == null ||
+        estimatedgrowth == null ||
+        averagefilesize == null ||
+        numfiles == null ||
+        tempcustomer == null){
+      return alert("Invalid Input")
+    }
+    alert("Information has been added to the database!")
+    //PT 1 sends information to the backend
+    Axios.post('http://localhost:3001/create',
+    {site:site,
+    location:location,
+    numofusers:numofusers,
+    totalamountofdata:totalamountofdata,
+    estimatedgrowth:estimatedgrowth,
+    averagefilesize:averagefilesize,
+    numfiles:numfiles,
+    tempcustomer:tempcustomer}).then(() => {
+      setEmployeeList([
+        ...employeeList,
+         {site:site,
+         location:location,
+         numofusers:numofusers,
+         totalamountofdata:totalamountofdata,
+         estimatedgrowth:estimatedgrowth,
+         averagefilesize:averagefilesize,
+         numfiles:numfiles, 
+         tempcustomer:tempcustomer
+         },
+       ])
+    })
+  };
+
+//////////////////////////////////////////////////////////////////////////
+  //Delete
+  const removeRow = () => {console.log(deleteSite);
+    if (deleteSite == null){
+      return alert("Invalid Input")
+    }
+    Axios.post('http://localhost:3001/delete',
+  {deleteSite:deleteSite});
+  getInfo()
+};
+/////////////////////////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+  //This is a function to get all rows from the database table sizer
+  const getInfo = () => {
+    Axios.get("http://localhost:3001/getinfo",).then((response) => {
+      setEmployeeList(response.data)
+    });
+  }
+////////////////////////////////////////////////////////////////////
+  //Function to get the searched for quality 
+  const searchItem = () => {console.log(searchSite);
+    if (searchSite == null){
+      return alert("Must input something")
+    }
+    Axios.post('http://localhost:3001/search',
+  {searchSite:searchSite}).then((response) => {
+    if (response.data.length == 0){
+      return alert("No row found")
+    }
+    console.log(response.data)
+    setEmployeeList(response.data)
+  })
+};
+
+/////////////////////////////////////////////////////////////////////////////
+  const addCustomer = () => {
+    if (customer == null) {
+      return alert("Cannot be empty")
+    } else {
+      Axios.post('http://localhost:3001/addCustomer',
+      {customer:customer,})
+    }
+  }
+/////////////////////////////////////////////////////////////////////////////
+//Physical display + onchange buttons for the information needed
+  return (
+    <div className="App">
+      <Paper elevation = {10} className = "page">
+
+        <div className = "info">
+            <Paper elevation = {10} className = "information">
+              <label>Customer</label>
+              <TextField type = "text"
+              onChange={(event) => {setCustomer(event.target.value)}}/>
+              <Button variant="outlined" onClick = {addCustomer}>Add</Button>
+            </Paper>
+        </div>
+
+        <div className = "information">
+          <Paper elevation = {10} className = "information">
+            <label>Site</label>
+            <TextField type = "number"
+            onChange={(event) => {setSite(event.target.value)}}/>
+            <label>Location</label>
+            <TextField type = "text"
+            onChange={(event) => {setLocation(event.target.value)}}/>
+            <label>Number of Users</label>
+            <TextField type = "number"
+            onChange={(event) => {setNumofusers(event.target.value)}}/>
+            <label>Total Amount of Data</label>
+            <TextField type = "number"
+            onChange={(event) => {setTotalamountofdata(event.target.value)}}/>
+            <label>Estimated Growth</label>
+            <TextField type = "number"
+            onChange={(event) => {setEstimatedgrowth(event.target.value)}}/>
+            <label>Average File Size</label>
+            <TextField type = "number"
+            onChange={(event) => {setAveragefilesize(event.target.value)}}/>
+            <label>Number of Files</label>
+            <TextField type = "number"
+            onChange={(event) => {setNumfiles(event.target.value)}}/>
+            <label>Customer</label>
+            <TextField type = "text"
+            onChange={(event) => {setTempCustomer(event.target.value)}}/>
+            <div className = 'AddButton'>
+              <Button variant="outlined" onClick = {addSite}>Add</Button>
+            </div>
+          </Paper>
+
+          <div className = "hideAndDelete">
+            <div>
+              <Paper elevation = {10}className = "delete">
+                <TextField type = "number"variant = "outlined"
+                onChange={(event) => {setDeletesite(event.target.value)}}/>
+                <Button  onClick = {removeRow} variant = "outlined">Delete</Button>
+              </Paper>
+            </div>
+
+            <div>
+              <Paper elevation = {10} className = "tablebutton">
+              <TextField type = "number"variant = "outlined"
+                onChange={(event) => {setSearchSite(event.target.value)}}/>
+                <Button  onClick = {searchItem} variant = "outlined">Search</Button>
+                <Button onClick = {getInfo} variant = "outlined">Show All</Button>
+              </Paper>
+            </div>
+          </div>
+        </div>
+
+          
+      <div>
+        <div className = "info" id = "info">
+        <TableContainer component={Paper} elevation = {10} className = "table">
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Site</TableCell>
+                <TableCell>Location</TableCell>
+                <TableCell>Number of Users</TableCell>
+                <TableCell>Total Amount of Data</TableCell>
+                <TableCell>Estimated Growth</TableCell>
+                <TableCell>Average File Size</TableCell>
+                <TableCell>Number of Files</TableCell>
+              </TableRow>
+            </TableHead>
+          {employeeList.map((val,key) => {
+            return <TableBody>
+              <TableRow>
+                <TableCell>{val.site}</TableCell>
+                <TableCell>{val.location}</TableCell>
+                <TableCell>{val.numofusers}</TableCell>
+                <TableCell>{val.totalamountofdata}</TableCell>
+                <TableCell>{val.estimatedgrowth}</TableCell>
+                <TableCell>{val.averagefilesize}</TableCell>
+                <TableCell>{val.numfiles}</TableCell>
+              </TableRow>
+            </TableBody>
+          })}
+          </Table>
+        </TableContainer>
+        </div>
+      </div>
+    </Paper>
+  </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,178 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2')
+const cors = require('cors');
+//const { setMaxListeners } = require('mysql2/typings/mysql/lib/Connection');
+//const { default: SelectInput } = require('@mui/material/Select/SelectInput');
+//const { setMaxListeners } = require('mysql2/typings/mysql/lib/Connection');
+
+app.use(cors())
+app.use(express.json());
+
+//setup connection to database
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: 'Chewey607',
+  database: 'examplesystem',
+});
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//When /create is used this data is created
+app.post('/create', (req,res) => {
+  //sizer table
+  console.log(req.body)
+  const site = req.body.site
+  const location = req.body.location
+  const numofusers = req.body.numofusers
+  const totalamountofdata = req.body.totalamountofdata
+  const estimatedgrowth = req.body.estimatedgrowth
+  const averagefilesize = req.body.averagefilesize
+  const numfiles = req.body.numfiles
+  const tempcustomer = req.body.tempcustomer
+  const freeze = 0
+
+  db.query("SELECT customerid FROM customer WHERE (customerName = ?)", [tempcustomer], (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      if (result[0] != null) {
+        var customerid = result[0]['customerid']
+      db.query("INSERT INTO quote (site, location, numusers, totaldata, estimatedgrowth, avgfilesize, numfiles, customerid, freeze) VALUES (?,?,?,?,?,?,?,?,?)",
+  [site, location, numofusers, totalamountofdata, estimatedgrowth, averagefilesize, numfiles, customerid, freeze]), (err, result) => {
+    if (err){
+      console.log(err)
+    } else {
+      res.send(result)
+    }
+  }
+}}
+  })
+});
+////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+//When /delete is used
+app.post('/delete', (req, res) => {
+  const bodySite = req.body.deleteSite
+
+  db.query("DELETE FROM sizer WHERE (site = ?)", [bodySite]), (err,result) => {
+    if (err){
+      console.log(err)
+    } 
+  }
+
+  db.query("DELETE FROM qidtab WHERE (site = ?)", [bodySite]), (err,result) => {
+    if (err){
+      console.log(err)
+    } 
+  }
+
+  db.query("SELECT * FROM sizer", (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(result)
+    };
+  });
+
+});
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+app.post('/addCustomer', (req,res) => {
+  //This function needs to accept multiple requests 
+    const customer = req.body.customer
+    var customerid = 0
+    console.log(req.body.customer)
+
+    //Query to check and see if the name is already in the database 
+    db.query("SELECT customerName from customer WHERE (customerName = ?)", [customer], (err, result) => {
+      //If the name is not in the database create the new name
+      if (result[0] == null){
+        //New query to create the customerid for the new customer name
+        db.query("SELECT max(customerid) from customer", (err, result) => {
+          //if the customerid is not null, then we add a number to the maximum customerid
+          if (result[0]['max(customerid)'] != null){
+            customerid = result[0]['max(customerid)'] + 1
+          }
+          //otherwise we add the error to the console
+          if (err) {
+            console.log(err)
+          } else {
+            //Otherwise query to insert a new line in the customer table inside the sql table
+            db.query('INSERT INTO customer (customerName, customerid) VALUES (?, ?)',
+      [customer, customerid]), (err, result) => {
+        if (err){
+          console.log(err)
+        }
+      }
+          }
+        })
+      } else {
+        //put something here to send back to tell the front end that this already exists within the database
+      }
+    })
+  });
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//When /search is used
+app.post('/search', (req, res) => {
+  const bodySite = req.body.searchSite
+  console.log(bodySite)
+  db.query("SELECT customerid FROM customer WHERE (customerName = ?)", [bodySite], (err,result) => {
+    var newcust = result[0]['customerid']
+    console.log(newcust)
+    if (err){
+      console.log(err)
+    } else {
+      db.query("SELECT site, location, numusers, totaldata, estimatedgrowth, avgfilesize, numfiles FROM quote WHERE (customerid = ?)", [newcust], (err, result) => {
+        if (err) {
+          console.log(err)
+        } else {
+          res.send(result)
+        }
+      })
+    }
+  })
+});
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//Takes every table from the MYSql table
+app.get('/getinfo', (req, res) => {
+  db.query("SELECT * FROM sizer", (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(result)
+    };
+  });
+});
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//Lets me know the server is running
+app.listen(3001, ()=> {
+  console.log("The server is running!")
+});
